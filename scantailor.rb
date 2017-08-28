a163d96b02f60883113ad921dd4104ba1f65b330
Mahmood Shafeie Zargar <mahmood@gmail.com>
2017-08-28 19:38:12 +0200

    scantailor complete

--- scantailor.rb
+++ scantailor.rb
@@ -2,13 +2,14 @@ require 'formula'
 
 class Scantailor < Formula
   homepage 'http://github.com/scantailor/scantailor'
-  head 'https://github.com/couchbase/geocouch.git'
+  head 'https://github.com/scantailor/scantailor.git'
   url 'https://github.com/scantailor/scantailor/archive/RELEASE_0_9_12_1.tar.gz'
   sha256 'ef5d5bdca207ab00701121a32e9b95c7c7353c642b9538b3f9ca040d8d1a5dde'
+  version = '0.9.12.1'
 
 
   depends_on 'cmake' => :build
-  depends_on 'cartr/qt4/qt@4'
+  depends_on 'cartr/qt4/qt@4' # tap cart/qt4 and install qt@4 formula (qt version 4) before installing
   depends_on 'boost'
   depends_on 'jpeg'
   depends_on 'libtiff'
@@ -18,5 +19,27 @@ class Scantailor < Formula
   def install
     system "cmake . #{std_cmake_parameters} -DPNG_INCLUDE_DIR=/usr/X11/include"
     system "make install"
+
+    # Create app bundle
+    pack = "packaging/osx"
+    app = "app/ScanTailor.app"
+    appcon = "#{app}/Contents"
+    macos = "#{appcon}/MacOS"
+    resources = "#{appcon}/Resources"
+    frameworks = "#{appcon}/Frameworks"
+    
+    
+    inreplace "#{pack}/Info.plist.in", "@VERSION@", version
+    mv "#{pack}/Info.plist.in", "#{pack}/Info.plist"
+    (buildpath/macos).mkpath
+    (buildpath/resources).mkpath
+    (buildpath/frameworks).mkpath
+    (buildpath/appcon).install "#{pack}/Info.plist"
+    (buildpath/macos).install "#{bin}/scantailor"
+    (buildpath/resources).install "#{pack}/ScanTailor.icns"
+    (buildpath/resources).install Dir["#{share}/scantailor/translations/scantailor_*.qm"]
+    prefix.install "#{buildpath}/#{app}"
+    bin.install_symlink prefix/"ScanTailor.app/Contents/MacOS/scantailor"
+    (share/"scantailor/translations").install_symlink Dir["#{prefix}/ScanTailor.app/Contents/Resources/scantailor_*.qm"]
   end
 end
